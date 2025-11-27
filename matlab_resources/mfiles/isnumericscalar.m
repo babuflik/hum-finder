d080d758e42fc3a3705d5e704723d409d675ae42
William Winbladh <william.winbladh@outlook.com>
2025-11-27 22:02:48 +0100

    Deleted matlab files

--- matlab_resources/mfiles/isnumericscalar.m
+++
@@ -1,9 +0,0 @@
-function ok=isnumericscalar(x)
-%ISNUMERICSCALAR tests if the argument is numeric and scalar
-%
-% ok=isnumscalar(x)
-
-% Copyright Fredrik Gustafsson
-%$ Revision: v2025.2 $
-
-ok = isnumeric(x) && isscalar(x);
